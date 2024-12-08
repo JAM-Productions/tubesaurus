3a8c6d0403b24f4aeb371236835614a398735304
mriverre8 <rivemark00@gmail.com>
2024-12-08 14:30:07 +0100

    prettier format

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ import DownloadForm from "@/components/DownloadForm";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-neutral-50 px-4">
-      <Header title={"Tubesaurus"}/>
+      <Header title={"Tubesaurus"} />
       <DownloadForm />
-      <Footer/>
+      <Footer />
     </main>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-function Header({title}: {title: string}) {
-    return (
-        <div className="mb-7 flex items-center space-x-2">
-        {/* TODO: ICON */}
-        <h1 className="text-2xl font-bold">{title}</h1>
-      </div>
-    );
-};
+function Header({ title }: { title: string }) {
+  return (
+    <div className="mb-7 flex items-center space-x-2">
+      {/* TODO: ICON */}
+      <h1 className="text-2xl font-bold">{title}</h1>
+    </div>
+  );
+}
 
 export default Header;
