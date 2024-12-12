02861c6288ae321c087a795677ad78ae6302aac4
Jordi <55429631+jorbush@users.noreply.github.com>
2024-12-12 17:54:29 +0100

    fix next lint error

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import React from "react";
+import Image from "next/image";
 
 function Header({ title }: { title: string }) {
   return (
     <div className="mb-7 flex items-center gap-1">
-      <img src="tubesaurus-logo.webp" alt="logo" className="h-20" />
+      <Image src="/tubesaurus-logo.webp" alt="logo" height={70} width={80} />
       <h1 className="text-4xl font-bold mt-5">{title}</h1>
     </div>
   );
